08eeac68afd3dad1e6b04f62f01cd3f4fe83050d
Javibu13 <javiersanzmoline@gmail.com>
2024-05-18 20:32:05 +0200

    Add initial parametrization

---
+++ doc/parametrizacionInicial.sql
@@ -0,0 +1,44 @@
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('Inception', 'Christopher Nolan', 'A thief who steals corporate secrets through the use of dream-sharing technology.', 'https://youtu.be/YoHD9XEInc0', 5, 'images/movies/inception.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('The Matrix', 'The Wachowskis', 'A computer hacker learns about the true nature of his reality and his role in the war against its controllers.', 'https://youtu.be/vKQi3bBA1y8', 3, 'images/movies/matrix.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('Interstellar', 'Christopher Nolan', 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity''s survival.', 'https://youtu.be/zSWdZVtXT7E', 4, 'images/movies/interestellar.png');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('The Godfather', 'Francis Ford Coppola', 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.', 'https://youtu.be/sY1S34973zA', 2, 'images/movies/thegodfather.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('Pulp Fiction', 'Quentin Tarantino', 'The lives of two mob hitmen, a boxer, a gangster''s wife, and a pair of diner bandits intertwine in four tales of violence and redemption.', 'https://youtu.be/s7EdQ4FqbhY', 6, 'images/movies/pulpfiction.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('The Shawshank Redemption', 'Frank Darabont', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 'https://youtu.be/6hB3S9bIaco', 4, 'images/movies/shawshank.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('The Dark Knight', 'Christopher Nolan', 'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.', 'https://youtu.be/EXeTwQWrcwY', 5, 'images/movies/thedarkknight.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('Forrest Gump', 'Robert Zemeckis', 'The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man.', 'https://youtu.be/bLvqoHBptjg', 3, 'images/movies/forrestgump.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('Fight Club', 'David Fincher', 'An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.', 'https://youtu.be/SUXWAEX2jlg', 4, 'images/movies/fightclub.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('The Lord of the Rings: The Fellowship of the Ring', 'Peter Jackson', 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth.', 'https://youtu.be/V75dMMIW2B4', 6, 'images/movies/tlotr.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('Star Wars: Episode V - The Empire Strikes Back', 'Irvin Kershner', 'After the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda.', 'https://youtu.be/JNwNXF9Y6kY', 5, 'images/movies/starwars.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('The Silence of the Lambs', 'Jonathan Demme', 'A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to catch another serial killer.', 'https://youtu.be/W6Mm8Sbe__o', 3, 'images/movies/silenceofthelambs.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('Schindler''s List', 'Steven Spielberg', 'In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce.', 'https://youtu.be/gG22XNhtnoY', 2, 'images/movies/schindler.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('8 Mile', 'Curtis Hanson', 'A young rapper, struggling with every aspect of his life, wants to make it big but his friends and foes make this odyssey of rap harder than it may seem.', 'https://www.youtube.com/watch?v=axGVrfwm9L4', 3, 'images/movies/8mile.jpg');
+
+INSERT INTO MOVIES (TITLE, DIRECTOR, SYNOPSIS, TRAILER, QUANTITY, PATH)
+VALUES ('Gladiator', 'Ridley Scott', 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', 'https://youtu.be/owK1qxDselE', 5, 'images/movies/gladiator.jpg');
